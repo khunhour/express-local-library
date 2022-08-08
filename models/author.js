380f2bf422f106ff3eebe6a05b9d636adc906525
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-08 21:16:29 +0900

    fix arrow function to normal function declaration

--- models/author.js
+++ models/author.js
@@ -1,28 +1,21 @@
 var mongoose = require("mongoose");
+
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema({
-	first_name: {
-		type: String,
-		required: true,
-		maxLength: 100,
-	},
-	family_name: {
-		type: String,
-		required: true,
-		maxLength: 100,
-	},
+	first_name: { type: String, required: true, maxLength: 100 },
+	family_name: { type: String, required: true, maxLength: 100 },
 	date_of_birth: { type: Date },
 	date_of_death: { type: Date },
 });
 
 // Virtual for author's full name
-AuthorSchema.virtual("name").get(() => {
+AuthorSchema.virtual("name").get(function () {
 	// To avoid errors in cases where an author does not have either a family name or first name
 	// We want to make sure we handle the exception by returning an empty string for that case
 	var fullname = "";
 	if (this.first_name && this.family_name) {
-		fullname = this.first_name + ", " + this.family_name;
+		fullname = this.family_name + ", " + this.first_name;
 	}
 	if (!this.first_name || !this.family_name) {
 		fullname = "";
@@ -31,7 +24,7 @@ AuthorSchema.virtual("name").get(() => {
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual("lifespan").get(() => {
+AuthorSchema.virtual("lifespan").get(function () {
 	var lifetime_string = "";
 	if (this.date_of_birth) {
 		lifetime_string = this.date_of_birth.getYear().toString();
@@ -44,9 +37,9 @@ AuthorSchema.virtual("lifespan").get(() => {
 });
 
 // Virtual for author's URL
-AuthorSchema.virtual("url").get(() => {
+AuthorSchema.virtual("url").get(function () {
 	return "/catalog/author/" + this._id;
 });
 
-// Export model
+//Export model
 module.exports = mongoose.model("Author", AuthorSchema);
