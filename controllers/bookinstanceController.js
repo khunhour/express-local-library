716133832248cbe3bbecbe5ab494cf59e02d54eb
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-11 21:27:58 +0900

    handle post request for deleting bookinstances

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -139,7 +139,13 @@ exports.bookinstance_delete_get = function (req, res) {
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function (req, res) {
-	res.send("NOT IMPLEMENTED: BookInstance delete POST");
+	BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+		if (err) {
+			return next(err);
+		}
+		// Success - go to book instance list
+		res.redirect("/catalog/bookinstances");
+	});
 };
 
 // Display BookInstance update form on GET.
