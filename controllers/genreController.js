702e1c0f13daf411f842985567043ba22cfa9e37
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-11 20:17:42 +0900

    handle get request on delete genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -104,8 +104,32 @@ exports.genre_create_post = [
 ];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function (req, res) {
-	res.send("NOT IMPLEMENTED: Genre delete GET");
+exports.genre_delete_get = function (req, res, next) {
+	async.parallel(
+		{
+			genre(callback) {
+				Genre.findById(req.params.id).exec(callback);
+			},
+			genres_books(callback) {
+				Book.find({ genre: req.params.id }).exec(callback);
+			},
+		},
+		function (err, results) {
+			if (err) {
+				return next(err);
+			}
+			if (results.genre == null) {
+				// No results.
+				res.redirect("/catalog/genres");
+			}
+			// Successful, so render.
+			res.render("genre_delete", {
+				title: "Delete Genre",
+				genre: results.genre,
+				genre_books: results.genres_books,
+			});
+		}
+	);
 };
 
 // Handle Genre delete on POST.
