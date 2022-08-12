44564a58c27ea58cf3ee3412030cb3592e3da11b
Khunhour Morm <khonhourmorm@gmail.com>
2022-08-12 20:05:50 +0900

    implemented update bookinstance on post request

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -189,3 +189,65 @@ exports.bookinstance_update_get = (req, res, next) => {
 exports.bookinstance_update_post = function (req, res) {
 	res.send("NOT IMPLEMENTED: BookInstance update POST");
 };
+
+// Handle bookinstance update on POST.
+exports.bookinstance_update_post = [
+	// Validate and sanitize fields.
+	body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+	body("imprint", "Imprint must be specified")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("status").escape(),
+	body("due_back", "Invalid date")
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		// Create a BookInstance object with escaped and trimmed data.
+		const bookinstance = new BookInstance({
+			book: req.body.book,
+			imprint: req.body.imprint,
+			status: req.body.status,
+			due_back: req.body.due_back,
+			_id: req.params.id, //This is required, or a new ID will be assigned!
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values and error messages.
+			Book.find({}, "title").exec(function (err, books) {
+				if (err) {
+					return next(err);
+				}
+				// Successful, so render.
+				res.render("bookinstance_form", {
+					title: "Create BookInstance",
+					book_list: books,
+					selected_book: bookinstance.book._id,
+					errors: errors.array(),
+					bookinstance,
+				});
+			});
+			return;
+		}
+
+		// Data from form is valid. Update the record.
+		BookInstance.findByIdAndUpdate(
+			req.params.id,
+			bookinstance,
+			{},
+			(err, thebookinstance) => {
+				if (err) {
+					return next(err);
+				}
+
+				// Successful: redirect to book detail page.
+				res.redirect(thebookinstance.url);
+			}
+		);
+	},
+];
